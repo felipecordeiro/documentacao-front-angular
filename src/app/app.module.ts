65385ce5113f2b1eec48b6a4fbe20756659d4210
Felipe Cordeiro Ramos <felipecordeiro4@gmail.com>
2021-11-01 21:15:26 -0300

    Adiciona componentes inputs + home + footer + header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputsComponent } from './inputs/inputs.component';
+import { HeaderComponentModule } from './components/header/header.component.module';
+import { FooterComponentModule } from './components/footer/footer.component.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HeaderComponentModule,
+    FooterComponentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
