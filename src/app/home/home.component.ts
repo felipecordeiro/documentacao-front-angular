61c9ce83e8ad406417835a34523962f205f07522
Felipe <felipe.c.ramos@hotmail.com>
2022-01-30 18:52:50 -0300

    Adiciona ngx-papaparse para processar arquivos csv

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Papa } from 'ngx-papaparse';
 
 @Component({
   selector: 'dfa-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private papa: Papa) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+      
+  }
+
+  fileChange(event) {
+    let file = event.target.files[0]
+    let fileReader = new FileReader()
+    fileReader.readAsText(file)
+    fileReader.onload = () => {
+      console.log('fileReader result ', fileReader.result)
+      this.papa.parse(fileReader.result.toString(), {
+        complete: r => {
+          console.log('Parsed: ', r)
+        }
+      })
+    }
+    fileReader.onerror = () => {
+      console.log('fileReader error ', fileReader.error)
+    }
+    console.log('file ', file)
+    
+    
   }
 
 }
