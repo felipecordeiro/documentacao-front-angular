d7e77ce207cb09c0acc7ff04fa37148fe183a194
Felipe <felipe.c.ramos@hotmail.com>
2022-02-26 01:45:59 -0300

    Position sticky header e footer

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'dfa-header',
-    templateUrl: 'header.component.html'
+    templateUrl: 'header.component.html',
+    styleUrls: ['header.component.scss']
 })
 
 export class HeaderComponent implements OnInit {
